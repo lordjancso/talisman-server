ddfd87ac5360580086a35844e6b1bce542d34b8c
lordjancso <lordjancso@gmail.com>
2015-09-15 20:17:53 +0200

    Draw random adventure card

---
+++ src/AppBundle/Controller/CardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class CardController extends Controller
+{
+    public function drawAdventureAction()
+    {
+        $em = $this->getDoctrine()->getManager('default');
+        $player = $em->getRepository('AppBundle:Player')->find(1);
+
+        $card = $em->getRepository('AppBundle:AdventureCard')->findRandom();
+
+        return new JsonResponse(array(
+            'id' => $card->getId(),
+            'name' => $card->getName(),
+            'description' => $card->getDescription(),
+            'image' => $card->getImage(),
+            'type' => $card->getType(),
+            'sub_type' => $card->getSubType(),
+            'strength' => $card->getStrength(),
+            'craft' => $card->getCraft(),
+            'encounter_number' => $card->getEncounterNumber(),
+        ));
+    }
+}
