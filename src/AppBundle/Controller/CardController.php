4b5df05bed1cf71be51fec284d6cc29058272113
lordjancso <lordjancso@gmail.com>
2015-09-22 08:00:27 +0200

    Draw multiple adventure cards

--- src/AppBundle/Controller/CardController.php
+++ src/AppBundle/Controller/CardController.php
@@ -7,28 +7,34 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CardController extends Controller
 {
-    public function drawAdventureAction()
+    public function drawAdventureAction($count)
     {
         $em = $this->getDoctrine()->getManager('default');
         $player = $em->getRepository('AppBundle:Player')->find(1);
 
-        $card = $em->getRepository('AppBundle:AdventureCard')->findRandom();
+        $cards = array();
 
-        $this->get('app.logger')->create($player, 'draw_adventure', $card);
+        for ($i = 0; $i < $count; $i++) {
+            $card = $em->getRepository('AppBundle:AdventureCard')->findRandom();
+
+            $cards[] = array(
+                'id' => $card->getId(),
+                'name' => $card->getName(),
+                'description' => $card->getDescription(),
+                'image' => $card->getImage(),
+                'type' => $card->getType(),
+                'sub_type' => $card->getSubType(),
+                'strength' => $card->getStrength(),
+                'craft' => $card->getCraft(),
+                'encounter_number' => $card->getEncounterNumber(),
+            );
+
+            $this->get('app.logger')->create($player, 'draw_adventure', $card);
+        }
 
         $em->flush();
 
-        return new JsonResponse(array(
-            'id' => $card->getId(),
-            'name' => $card->getName(),
-            'description' => $card->getDescription(),
-            'image' => $card->getImage(),
-            'type' => $card->getType(),
-            'sub_type' => $card->getSubType(),
-            'strength' => $card->getStrength(),
-            'craft' => $card->getCraft(),
-            'encounter_number' => $card->getEncounterNumber(),
-        ));
+        return new JsonResponse($cards);
     }
 
     public function acceptAction($id)
