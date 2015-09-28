f6bfcf829b75dac7f202de800a406ced0da3b8a5
lordjancso <lordjancso@gmail.com>
2015-09-28 07:45:43 +0200

    Field informations added

--- src/AppBundle/Board/Space.php
+++ src/AppBundle/Board/Space.php
@@ -10,55 +10,194 @@ class Space
      * @var array
      */
     private static $graph = array(
-        1 => array(2, 24),
-        2 => array(1, 3),
-        3 => array(2, 4),
-        4 => array(3, 5),
-        5 => array(4, 6),
-        6 => array(5, 7),
-        7 => array(6, 8),
-        8 => array(7, 9),
-        9 => array(8, 10),
-        10 => array(9, 11),
-        11 => array(10, 12),
-        12 => array(11, 13),
-        13 => array(12, 14),
-        14 => array(13, 15),
-        15 => array(14, 16),
-        16 => array(15, 17),
-        17 => array(16, 18, 36),
-        18 => array(17, 19),
-        19 => array(18, 20),
-        20 => array(19, 21),
-        21 => array(20, 22),
-        22 => array(21, 23),
-        23 => array(22, 24),
-        24 => array(1, 23),
-        25 => array(26, 40),
-        26 => array(25, 27),
-        27 => array(26, 28),
-        28 => array(27, 29),
-        29 => array(28, 30),
-        30 => array(29, 31),
-        31 => array(30, 32),
-        32 => array(31, 33),
-        33 => array(32, 34, 45),
-        34 => array(33, 35),
-        35 => array(34, 36),
-        36 => array(17, 35, 37),
-        37 => array(36, 38),
-        38 => array(37, 39),
-        39 => array(38, 40),
-        40 => array(25, 39),
-        41 => array(42, 48, 49),
-        42 => array(41, 43),
-        43 => array(42, 44),
-        44 => array(43, 45),
-        45 => array(33, 44, 46),
-        46 => array(45, 47),
-        47 => array(46, 48),
-        48 => array(41, 47),
-        49 => array(41),
+        1 => array(
+            'connections' => array(2, 24),
+            'name' => 'City',
+        ),
+        2 => array(
+            'connections' => array(1, 3),
+            'draw' => 1,
+            'name' => 'Fields',
+        ),
+        3 => array(
+            'connections' => array(2, 4),
+            'draw' => 1,
+            'name' => 'Hills',
+        ),
+        4 => array(
+            'connections' => array(3, 5),
+            'draw' => 1,
+            'name' => 'Plains',
+        ),
+        5 => array(
+            'connections' => array(4, 6),
+            'draw' => 1,
+            'name' => 'Woods',
+        ),
+        6 => array(
+            'connections' => array(5, 7),
+            'draw' => 1,
+            'name' => 'Plains',
+        ),
+        7 => array(
+            'connections' => array(6, 8),
+            'name' => 'Tavern',
+        ),
+        8 => array(
+            'connections' => array(7, 9),
+            'draw' => 1,
+            'name' => 'Fields',
+        ),
+        9 => array(
+            'connections' => array(8, 10),
+            'draw' => 2,
+            'name' => 'Ruins',
+        ),
+        10 => array(
+            'connections' => array(9, 11),
+            'draw' => 1,
+            'name' => 'Plains',
+        ),
+        11 => array(
+            'connections' => array(10, 12),
+            'name' => 'Forest',
+        ),
+        12 => array(
+            'connections' => array(11, 13),
+            'draw' => 1,
+            'name' => 'Fields',
+        ),
+        13 => array(
+            'connections' => array(12, 14),
+            'name' => 'Village',
+        ),
+        14 => array(
+            'connections' => array(13, 15),
+            'draw' => 1,
+            'name' => 'Fields',
+        ),
+        15 => array(
+            'connections' => array(14, 16),
+            'name' => 'Graveyard',
+        ),
+        16 => array(
+            'connections' => array(15, 17),
+            'draw' => 1,
+            'name' => 'Woods',
+        ),
+        17 => array(
+            'connections' => array(16, 18, 36),
+            'name' => 'Sentinel',
+        ),
+        18 => array(
+            'connections' => array(17, 19),
+            'draw' => 1,
+            'name' => 'Hills',
+        ),
+        19 => array(
+            'connections' => array(18, 20),
+            'name' => 'Chapel',
+        ),
+        20 => array(
+            'connections' => array(19, 21),
+            'draw' => 1,
+            'name' => 'Fields',
+        ),
+        21 => array(
+            'connections' => array(20, 22),
+            'name' => 'Crags',
+        ),
+        22 => array(
+            'connections' => array(21, 23),
+            'draw' => 1,
+            'name' => 'Plains',
+        ),
+        23 => array(
+            'connections' => array(22, 24),
+            'draw' => 1,
+            'name' => 'Woods',
+        ),
+        24 => array(
+            'connections' => array(1, 23),
+            'draw' => 1,
+            'name' => 'Fields',
+        ),
+        25 => array(
+            'connections' => array(26, 40),
+            'name' => 'Warlock\'s Cave',
+        ),
+        26 => array(
+            'connections' => array(25, 27),
+        ),
+        27 => array(
+            'connections' => array(26, 28),
+        ),
+        28 => array(
+            'connections' => array(27, 29),
+        ),
+        29 => array(
+            'connections' => array(28, 30),
+        ),
+        30 => array(
+            'connections' => array(29, 31),
+        ),
+        31 => array(
+            'connections' => array(30, 32),
+        ),
+        32 => array(
+            'connections' => array(31, 33),
+        ),
+        33 => array(
+            'connections' => array(32, 34, 45),
+        ),
+        34 => array(
+            'connections' => array(33, 35),
+        ),
+        35 => array(
+            'connections' => array(34, 36),
+        ),
+        36 => array(
+            'connections' => array(17, 35, 37),
+        ),
+        37 => array(
+            'connections' => array(36, 38),
+        ),
+        38 => array(
+            'connections' => array(37, 39),
+        ),
+        39 => array(
+            'connections' => array(38, 40),
+        ),
+        40 => array(
+            'connections' => array(25, 39),
+        ),
+        41 => array(
+            'connections' => array(42, 48, 49),
+        ),
+        42 => array(
+            'connections' => array(41, 43),
+        ),
+        43 => array(
+            'connections' => array(42, 44),
+        ),
+        44 => array(
+            'connections' => array(43, 45),
+        ),
+        45 => array(
+            'connections' => array(33, 44, 46),
+        ),
+        46 => array(
+            'connections' => array(45, 47),
+        ),
+        47 => array(
+            'connections' => array(46, 48),
+        ),
+        48 => array(
+            'connections' => array(41, 47),
+        ),
+        49 => array(
+            'connections' => array(41),
+        )
     );
 
     /**
@@ -72,14 +211,19 @@ class Space
     }
 
     /**
-     * Return a single field.
+     * Return a single property of a single field.
      *
-     * @param $id
+     * @param int $id
+     * @param string $name
      *
-     * @return array
+     * @return mixed
      */
-    public static function get($id)
+    public static function get($id, $name)
     {
-        return self::$graph[$id];
+        if (isset(self::$graph[$id][$name])) {
+            return self::$graph[$id][$name];
+        }
+
+        return false;
     }
 }
