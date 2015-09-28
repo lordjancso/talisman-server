a06223a1d1aa90908fdb04fa824dc0ab3cfe2c8b
lordjancso <lordjancso@gmail.com>
2015-09-28 07:47:25 +0200

    Get board informations fix

--- src/AppBundle/Service/MoveService.php
+++ src/AppBundle/Service/MoveService.php
@@ -6,9 +6,19 @@ use AppBundle\Board\Space;
 
 class MoveService
 {
+    /**
+     * Calculate possible destinations of a player by roll value and current position.
+     *
+     * @param $position int
+     * @param $roll int
+     * @param $from null|int
+     * @param $destinations array
+     *
+     * @return array
+     */
     public function getPossibleDestinationsByRoll($position, $roll, $from = null, &$destinations = array())
     {
-        $current = Space::get($position);
+        $current = Space::get($position, 'connections');
         $roll = $roll - 1;
 
         foreach ($current as $next) {
