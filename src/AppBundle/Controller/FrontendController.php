e13e245102c1ed09b8a82040de187024bc99c1d3
lordjancso <lordjancso@gmail.com>
2015-10-03 10:17:25 +0200

    Board changes

--- src/AppBundle/Controller/FrontendController.php
+++ src/AppBundle/Controller/FrontendController.php
@@ -2,12 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Board\Space;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class FrontendController extends Controller
 {
     public function startAction()
     {
-        return $this->render('board/index.html.twig');
+        return $this->render('board/index.html.twig', array(
+            'spaces' => Space::all(),
+        ));
     }
 }
